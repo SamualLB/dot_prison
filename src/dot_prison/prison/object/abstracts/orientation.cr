bdfcc2d71c98833e9ad848a24f6973e730d27d2d
Samual Black <samuallb@hotmail.com>
2019-02-18 14:55:32 +0000

    Orientation module

--- src/dot_prison/prison/object/abstracts/orientation.cr
+++ src/dot_prison/prison/object/abstracts/orientation.cr
@@ -10,19 +10,42 @@ module DotPrison::Prison::Object::Orientation
     @or_y = store.parse_float("Or.y")
   end
 
-  # Angle between 0 and 2 PI radians from top
   def orientation : Float64
-    @orientation ||= find_angle(@or_x, @or_y) / Math::PI
+    @orientation ||= Orientation.find_angle(@or_x, @or_y)
   end
 
-  private def find_angle(x, y)
+  def orientation=(new_o : Float64)
+    @orientation = new_o
+    @or_x, @or_y = Orientation.convert_angle(new_o)
+  end
+
+  def orientation_x=(new : Float64)
+    @or_x = new
+    @orientation = nil
+  end
+
+  def orientation_y=(new : Float64)
+    @or_y = new
+    @orientation = nil
+  end
+
+  def orientation=(new : Tuple(Float64, Float64))
+    @or_x = new[0]
+    @or_y = new[1]
+    @orientation = nil
+  end
+
+  # Parse PA x, y format to 0..2PI radians
+  #
+  # From top of screen clockwise
+  protected def self.find_angle(x, y)
     res = Math.atan2(x, -y)
     res += 2*Math::PI if res < 0.0
     res
   end
 
-  # Needs to be in radians... not PI radians
-  def convert_angle(ang)
+  # Convert back to PA format
+  protected def self.convert_angle(ang) : Tuple(Float64, Float64)
     {Math.sin(ang), -Math.cos(ang)}
   end
 end
