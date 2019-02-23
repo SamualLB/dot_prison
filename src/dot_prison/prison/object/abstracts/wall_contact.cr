39d84555b747ee7d3e3cb41d4fc5a017b0eec5c4
Samual Black <samuallb@hotmail.com>
2019-02-23 11:17:32 +0000

    Wall contact parsing

--- src/dot_prison/prison/object/abstracts/wall_contact.cr
+++ src/dot_prison/prison/object/abstracts/wall_contact.cr
@@ -1,10 +1,24 @@
 module DotPrison::Prison::Object::WallContact
-  @wall_contact_x = 0.0
-  @wall_contact_y = 0.0
+  @wall_contact_x = LRContact::None
+  @wall_contact_y = UDContact::None
 
   def initialize(prison : Prison, store : Store)
     super
-    @wall_contact_x = store.parse_float("Walls.x")
-    @wall_contact_y = store.parse_float("Walls.y")
+    x_flt = store.parse_float("Walls.x")
+    @wall_contact_x = LRContact.from_value?(x_flt.to_i) || LRContact::None
+    y_flt = store.parse_float("Walls.y")
+    @wall_contact_y = UDContact.from_value?(y_flt.to_i) || UDContact::None
+  end
+
+  enum UDContact
+    None    = 0
+    Up      = -1
+    Down    = 1
+  end
+
+  enum LRContact
+    None    = 0
+    Left    = -1
+    Right   = 1
   end
 end
