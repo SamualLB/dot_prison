26a7272d804a3a306aff1ebd506561f9a63627c8
Samual Black <samuallb@hotmail.com>
2019-02-24 20:23:26 +0000

    Serving table slots

--- src/dot_prison/prison/object/serving_table.cr
+++ src/dot_prison/prison/object/serving_table.cr
@@ -1,5 +1,10 @@
 class DotPrison::Prison::Object::ServingTable < DotPrison::Prison::Object
+  NO_SLOTS = 5
+
+  include Slot
+
   def initialize(prison : Prison, store : Store)
     super
+    puts @slots.size
   end
 end
