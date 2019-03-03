041db9afad30fee4e569a4106f46b47e2f0c1942
Samual Black <samuallb@hotmail.com>
2019-03-03 11:39:32 +0000

    Prisoner properties complete

--- src/dot_prison/prison/object/people/prisoner.cr
+++ src/dot_prison/prison/object/people/prisoner.cr
@@ -1,5 +1,5 @@
 class DotPrison::Prison::Object::Prisoner < DotPrison::Prison::Object
-  HANDLED_PROPERTIES.push "Experience", "StatusEffects", "Needs", "Bio", "TimeOfLastMisconduct", "LastAte", "BoilingPoint", "Category", "Timer", "Cell.i", "Cell.u", "Energy", "CarrierId.i", "CarrierId.u", "AiWalkSpeed", "Loaded", "AvailableMoney", "Station.i", "Station.u", "Dest.x", "Dest.y", "Vel.x", "Vel.y", "LastVisitors"
+  HANDLED_PROPERTIES.push "Experience", "StatusEffects", "Needs", "Bio", "TimeOfLastMisconduct", "LastAte", "BoilingPoint", "Category", "Timer", "Cell.i", "Cell.u", "Energy", "CarrierId.i", "CarrierId.u", "AiWalkSpeed", "Loaded", "AvailableMoney", "Station.i", "Station.u", "Dest.x", "Dest.y", "Vel.x", "Vel.y", "LastVisitors", "AiSetTarget", "JobId", "LastTreated", "AttackTimer", "Damage", "RequiredCellType", "Shackled", "Carried", "Carrying.i", "Carrying.u", "SnitchTimer", "Naked", "BookInCell", "CarryingBook", "Library.i", "Library.u"
 
   include Orientation
 
@@ -8,6 +8,8 @@ class DotPrison::Prison::Object::Prisoner < DotPrison::Prison::Object
   @needs : Store
   @bio : Store
   @timer : Float64
+  @required_cell_type : String
+  @carried : Int32
 
   property last_misconduct : Float64
   property last_ate : Float64
@@ -23,6 +25,18 @@ class DotPrison::Prison::Object::Prisoner < DotPrison::Prison::Object
   property destination : Tuple(Float64, Float64)
   property velocity : Tuple(Float64, Float64)
   property last_visitors : Float64
+  property ai_set_target : Bool
+  property job_id : Int32
+  property last_treated : Float64
+  property attack_timer : Float64
+  property damage : Float64
+  property shackled : Bool
+  property carrying : Reference(Object)
+  property snitch_timer : Float64
+  property naked : Bool
+  property book_in_cell : Bool
+  property carrying_book : Bool
+  property library : Reference(Room)
 
   def initialize(prison : Prison, store : Store)
     super
@@ -31,25 +45,35 @@ class DotPrison::Prison::Object::Prisoner < DotPrison::Prison::Object
     @needs = store.parse_store("Needs")
     @bio = store.parse_store("Bio")
     @timer = store.parse_float("Timer")
+    @required_cell_type = store.parse_string("Solitary") || "Unset"
 
     @last_misconduct = store.parse_float("TimeOfLastMisconduct")
     @last_ate = store.parse_float("LastAte")
     @boiling_point = store.parse_float("BoilingPoint")
     @category = Category.from_store(store.parse_string("Category"))
-    #@cell = CellReference.new(prison, store.parse_int("Cell.i"), store.parse_int("Cell.u"))
     @cell = Reference(Cell).new(prison, store.parse_int("Cell.i"), store.parse_int("Cell.u"))
     @energy = store.parse_float("Energy")
-    #@carrier = CarrierReference.new(prison, store.parse_int("CarrierId.i"), store.parse_int("CarrierId.u"))
     @carrier = Reference(Object).new(prison, store.parse_int("CarrierId.i"), store.parse_int("CarrierId.u"))
     @walk_speed = store.parse_float("AiWalkSpeed")
     @loaded = store.parse_bool("Loaded")
     @money = store.parse_float("AvailableMoney")
-    #@station = StationReference.new(prison, store.parse_int("Station.i"), store.parse_int("Station.u"))
     @station = Reference(Room).new(prison, store.parse_int("Station.i"), store.parse_int("Station.u"))
     @destination = {store.parse_float("Dest.x"), store.parse_float("Dest.y")}
     @velocity = {store.parse_float("Vel.x"), store.parse_float("Vel.y")}
     @last_visitors = store.parse_float("LastVisitors")
-    @station.target
+    @ai_set_target = store.parse_bool("AiSetTarget")
+    @job_id = store.parse_int("JobId")
+    @last_treated = store.parse_float("LastTreated")
+    @attack_timer = store.parse_float("AttackTimer")
+    @damage = store.parse_float("Damage")
+    @shackled = store.parse_bool("Shackled")
+    @carried = store.parse_int("Carried")
+    @carrying = Reference(Object).new(prison, store.parse_int("Carrying.i"), store.parse_int("Carrying.u"))
+    @snitch_timer = store.parse_float("SnitchTimer")
+    @naked = store.parse_bool("Naked")
+    @book_in_cell = store.parse_bool("BookInCell")
+    @carrying_book = store.parse_bool("CarryingBook")
+    @library = Reference(Room).new(prison, store.parse_int("Library.i"), store.parse_int("Library.u"))
   end
 
   enum Category
