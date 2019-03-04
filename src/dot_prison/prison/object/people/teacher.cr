7919ad131088c3b4d9c3de7ead93722531c25948
Samual Black <samuallb@hotmail.com>
2019-03-04 11:59:54 +0000

    External teacher

--- src/dot_prison/prison/object/people/teacher.cr
+++ src/dot_prison/prison/object/people/teacher.cr
@@ -1,5 +1,10 @@
 class DotPrison::Prison::Object::Teacher < DotPrison::Prison::Object
+  HANDLED_PROPERTIES.push "IsExternalTeacher"
+
+  property external : Bool
+
   def initialize(prison : Prison, store : Store)
     super
+    @external = store.parse_bool("IsExternalTeacher")
   end
 end
