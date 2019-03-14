d16b6c522ac5355debd0d2510af081e83d10dac1
Samual Black <samuallb@hotmail.com>
2019-03-14 21:49:30 +0000

    Door timer active times

--- src/dot_prison/prison/object/door_timer.cr
+++ src/dot_prison/prison/object/door_timer.cr
@@ -3,8 +3,22 @@ class DotPrison::Prison::Object::DoorTimer < DotPrison::Prison::Object
   include Powered
 
   handle(:doors_open, :Bool, :DoorsOpen)
+  handle(:regime_category, :Int32, :RegimeCategory)
+
+  property! active_times : Hash(Int32, Bool)
+
+  TIMES = [] of Symbol
+  {% for i in 0...24 %}
+    TIMES << :Time{{i}}
+    no_handle(:Time{{i}})
+  {% end %}
 
   def initialize(store : Store, prison : Prison)
     super
+    active_times = Hash(Int32, Bool).new
+    TIMES.each_with_index do |key, i|
+      active_times[i] = store.parse_bool(key)
+    end
+    puts active_times
   end
 end
