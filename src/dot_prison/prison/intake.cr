b682c9f3d74763e39390edbc46f8023428b0f4e0
Samual Black <samuallb@hotmail.com>
2020-07-12 09:54:05 +0100

    Intake category enum

--- src/dot_prison/prison/intake.cr
+++ src/dot_prison/prison/intake.cr
@@ -1,7 +1,16 @@
 struct DotPrison::Prison::Intake < DotPrison::Consumer
   struct Category < DotPrison::Consumer
-    # TODO: Enum it
-    consume :category, String, :PrisonerCategory
+    enum Type
+      MinSec
+      Normal
+      MaxSec
+      SuperMax
+      DeathRow
+      Insane
+      PrisonerTransfer
+    end
+
+    consume :category, Type, :PrisonerCategory
     consume :pool, Int32, :Pool
     consume :ratio, Float64, :Ratio
     consume :next_intake, Int32, :NextIntake
@@ -9,7 +18,7 @@ struct DotPrison::Prison::Intake < DotPrison::Consumer
     consume :num_nitg, Int32, :NumNITGs
   end
 
-  # TODO: Enum it!
+  # TODO: Enum int!
   consume :type, Int32, :IntakeType
   consume :num, Int32, :IntakeNum
   consume :death_row_blocked, Int32, :DeathRowBlocked
